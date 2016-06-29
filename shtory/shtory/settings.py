36efd080f7088c18cacbac8bd47310f53c4a6bb2
hbhalla123 <hbhalla95@gmail.com>
2016-06-29 10:36:44 +0530

    updated

--- shtory/shtory/settings.py
+++ shtory/shtory/settings.py
@@ -127,6 +127,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rkap95@gmail.com'
-EMAIL_HOST_PASSWORD = 'imstraightforward'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'rkap95@gmail.com'
 DEFAULT_TO_EMAIL = 'bhallahimank@gmail.com'
